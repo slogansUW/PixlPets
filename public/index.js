92f5b4e13491413e3523a75b3352c6159e59f2cf
slogansUW <slogans@uw.edu>
2023-02-02 13:41:51 -0800

    initial commit

--- public/index.js
+++ public/index.js
@@ -5,8 +5,10 @@ const result = document.querySelector("#result");
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
   const pixlpetId = document.querySelector("#pixlpetId").value;
+  console.log('pixlpetId:', pixlpetId);
   try {
     const response = await axios.get(`https://www.thepixlverse.io/pixlpets/${pixlpetId}`);
+    console.log(response)
     result.innerHTML = JSON.stringify(response.data);
   } catch (error) {
     result.innerHTML = "Error: " + error.message;
