9f4e063fde7026b5b4d296dd0e2021ef8640975e
slogansUW <slogans@uw.edu>
2023-02-02 13:57:49 -0800

    basic functional

--- public/index.js
+++ public/index.js
@@ -1,15 +1,13 @@
-const axios = require("axios");
 const form = document.querySelector("form");
 const result = document.querySelector("#result");
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
   const pixlpetId = document.querySelector("#pixlpetId").value;
-  console.log('pixlpetId:', pixlpetId);
   try {
-    const response = await axios.get(`https://www.thepixlverse.io/pixlpets/${pixlpetId}`);
-    console.log(response)
-    result.innerHTML = JSON.stringify(response.data);
+    const response = await fetch(`https://www.thepixlverse.io/pixlpets/${pixlpetId}`);
+    const data = await response.json()
+    result.innerHTML = JSON.stringify(data);
   } catch (error) {
     result.innerHTML = "Error: " + error.message;
   }
