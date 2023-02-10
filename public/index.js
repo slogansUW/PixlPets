22682af4b0919af11f883cc1e51368d7df6a4003
slogansUW <slogans@uw.edu>
2023-02-09 21:56:06 -0800

    Resized text, adjusted spacing, added logic for images for queried pet

--- public/index.js
+++ public/index.js
@@ -10,10 +10,38 @@ form.addEventListener("submit", async (event) => {
     const isHatched = data.attributes.some(
       (attribute) => attribute.trait_type === "Egg" && attribute.value === "Hatched"
     );
+
+    let earthImage = "images/earth.webp";
+    let fireImage = "images/fire.webp";
+    let airImage = "images/air.webp";
+    let waterImage = "images/water.webp";
+    let imageHTML = "";
+    
     if (isHatched) {
-      result.innerHTML = "Query for Pixl Pet #" + pixlpetId + "<br><br>Don't get bamboozled. The eggs been hatched anon, refresh the meta data.<br>ARF";
+      result.innerHTML = "Query for Pixl Pet #" + pixlpetId + 
+                          "<br><br>Don't get bamboozled. The eggs been hatched anon, refresh the meta data.<br>ARF" +
+                          "<br><br><img src='" + data.image + "' alt='Pixl Pet Image'>";
     } else {
-      result.innerHTML = "Query for Pixl Pet #" + pixlpetId + "<br><br>Anon, its not hatched. Don't be a coward, scoop it before they hatch.<br>ARF";
+        result.innerHTML = "Query for Pixl Pet #" + pixlpetId + "<br><br>Anon, its not hatched. Don't be a coward, scoop it before they hatch.<br>ARF<br><br>";
+        data.attributes.forEach((attribute) => {
+          if (attribute.trait_type === "Element") {
+            switch (attribute.value) {
+              case "Earth":
+                imageHTML = "<img src='" + earthImage + "' alt='Earth Element'>";
+                break;
+              case "Fire":
+                imageHTML = "<img src='" + fireImage + "' alt='Fire Element'>";
+                break;
+              case "Air":
+                imageHTML = "<img src='" + airImage + "' alt='Air Element'>";
+                break;
+              case "Water":
+                imageHTML = "<img src='" + waterImage + "' alt='Water Element'>";
+                break;
+            }
+          }
+        });
+        result.innerHTML += imageHTML;
     }
     
   } catch (error) {
